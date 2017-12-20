6d20bcbfccda24cedf9a27734cc03f5c8516cdc1
Beatriz <biacris5@yahoo.com.br>
2017-12-20 13:03:12 -0200

    Created a game

---
+++ src/main.py
@@ -0,0 +1,33 @@
+"""
+O tesouro Inca de Mamaquilla
+"""
+from random import shuffle
+CIRCLE = 9310
+class Baralho:
+    """
+    cartas do jogo
+    """
+    def __init__(self, jogo):
+        self.jogo = jogo
+        self.baralho = [carta for carta in range(1, 31)]
+        shuffle(self.baralho)
+        for carta in self.baralho:
+            if self.jogo.decide():
+                print("terminou")
+                break
+            self.jogo.apresenta_carta(carta)
+class Jogo:
+    """
+    O jogo do tesouro inca
+    """
+    def __init__(self):
+        self.mapa = {c: chr(CIRCLE+c) for c in range(1,16)}
+        self.baralho = Baralho(self)
+    def apresenta_carta(self, carta):
+        carta = self.mapa[carta] if carta in self.mapa else carta
+        print(carta,end=" ")
+    def decide(self):
+        decidiu = input("(s)sai ou (f)fica")
+        return decidiu == "s"
+if __name__ == "__main__":
+    Jogo()
